26c3c318ff696cb569143843b1888c482527f177
Amuri Bonface <amuribonface@gmail.com>
2019-07-30 01:52:25 +0300

    change package name

---
+++ app/src/main/java/com/news/myworld/allnews/util/UtilityMethods.java
@@ -0,0 +1,20 @@
+package com.news.myworld.allnews.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+import com.news.myworld.allnews.MyTimesApplication;
+
+public class UtilityMethods {
+    /**
+     * Method to detect network connection on the device
+     */
+    public static boolean isNetworkAvailable() {
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) MyTimesApplication.getMyTimesApplicationInstance()
+                        .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return connectivityManager.getActiveNetworkInfo() != null
+                && connectivityManager.getActiveNetworkInfo().isConnectedOrConnecting();
+    }
+}
